cb22a751242c620a730c94b810dc0f6715ed0d32
corytran <cktran02@gmail.com>
2021-01-10 22:40:25 -0800

    1.3.3
    fixed player-cursor angle, tweaked player/enemy values and rates

--- Assets/PlayerAngle.cs
+++ Assets/PlayerAngle.cs
@@ -4,31 +4,24 @@ using UnityEngine;
 
 public class PlayerAngle : MonoBehaviour
 {
+ 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(
+        mousePosition.x - transform.position.x,
+        mousePosition.y - transform.position.y
+        );
 
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-        //Ta Daaa
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-    }
-
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        transform.right = -direction;
     }
 }
--- Assets/PlayerProjectileAngle.cs
+++ Assets/PlayerProjectileAngle.cs
@@ -8,28 +8,19 @@ public class PlayerProjectileAngle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(
+        mousePosition.x - transform.position.x,
+        mousePosition.y - transform.position.y
+        );
 
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-        //Ta Daaa
-
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90));
+        transform.up = direction;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
