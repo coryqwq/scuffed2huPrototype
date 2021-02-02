aed019e3c0450e9507053dbcd8280a8dd3d46d52
corytran <cktran02@gmail.com>
2021-02-01 20:25:26 -0800

    1.11.2
    bug fixes

--- Assets/Player Scripts/FireGun2.cs
+++ Assets/Player Scripts/FireGun2.cs
@@ -12,11 +12,9 @@ public class FireGun2 : MonoBehaviour
     public float bulletSpread = 10.0f;
     public int bulletDamage = 3;
 
-    public bool invoked1;
-    public bool invoked2;
+    public bool invoked;
 
     public GameObject projectilePrefab;
-
     void Update()
     {
         PlayerController playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
@@ -26,34 +24,21 @@ public class FireGun2 : MonoBehaviour
         //reset values when the player is not holding the fire button, so next time, start with first round of shotgun fire
         if (Input.GetMouseButton(0) == false)
         {
-            CancelInvoke("Fire1");
-            CancelInvoke("Fire2");
-            invoked1 = false;
-            invoked2 = true;
+            CancelInvoke("FireGun");
+            invoked = false;
         }
          gunManagerScript.timer += Time.deltaTime;
-        //first rounds of shotgun fire
-        if (Input.GetMouseButton(0) && playerControllerScript.ammoCount2 > 0 && invoked1 == false && gunManagerScript.timer >= 1)
+        //rounds of shotgun fire
+        if (Input.GetMouseButton(0) && playerControllerScript.ammoCount2 > 0 && invoked == false && gunManagerScript.timer >= 1)
         {
-            InvokeRepeating("Fire1", startDelay1, spawnInterval1);
-            InvokeRepeating("Fire1", startDelay2, spawnInterval2);
+            InvokeRepeating("FireGun", startDelay1, spawnInterval1);
+            InvokeRepeating("FireGun", startDelay2, spawnInterval2);
             gunManagerScript.timer = 0;
-            StartCoroutine(Delay1(0.2f));
-            invoked1 = true;
-            invoked2 = false;
-        }
-
-        //every round of shotgun fire after the first rounds while still holding the fire button
-        if (Input.GetMouseButton(0) && playerControllerScript.ammoCount2 > 0 && invoked2 == false)
-        {
-            InvokeRepeating("Fire2", spawnInterval1 + 0.05f, spawnInterval1);
-            InvokeRepeating("Fire2", spawnInterval2 + 0.2f, spawnInterval2);
-
-            invoked2 = true;
+            invoked = true;
         }
     }
     // Update is called once per frame
-    void Fire1()
+    void FireGun()
     {
 
         PlayerController playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
@@ -71,31 +56,5 @@ public class FireGun2 : MonoBehaviour
                    transform.rotation * Quaternion.AngleAxis(-bulletSpread, Vector3.forward));
 
         playerControllerScript.ammoCount2 -= 3;
-    }
-    void Fire2()
-    {
-
-        PlayerController playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-
-        //play shotgun sfx
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.Play();
-
-        //create projectiles
-        Instantiate(projectilePrefab, transform.position + new Vector3(0, 0, 2),
-                   transform.rotation * Quaternion.AngleAxis(0, Vector3.forward));
-        Instantiate(projectilePrefab, transform.position + new Vector3(0, 0, 2),
-                   transform.rotation * Quaternion.AngleAxis(bulletSpread, Vector3.forward));
-        Instantiate(projectilePrefab, transform.position + new Vector3(0, 0, 2),
-                   transform.rotation * Quaternion.AngleAxis(-bulletSpread, Vector3.forward));
-
-        playerControllerScript.ammoCount2 -= 3;
-    }
-
-    IEnumerator Delay1(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        CancelInvoke("FireGun");
-        
     }
 }
